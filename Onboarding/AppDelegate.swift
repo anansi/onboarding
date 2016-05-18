b54ed8200832c5044828b2260de3d5aa2b5c688b
Julian Hulme <julian@arcadesoftware.io>
2016-05-18 12:45:05 +0200

    added the session view controller and the dashboard screen

--- Onboarding/AppDelegate.swift
+++ Onboarding/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
+        let dashboardViewController = DashboardViewController(nibName:"DashboardViewController",bundle:nil)
+        self.sessionNavigationController = UINavigationController(rootViewController: dashboardViewController)
+        self.sessionNavigationController?.navigationBarHidden = true
         
         let landingViewController = LandingViewController(nibName:"LandingViewController",bundle:nil)
         self.landingNavigationController = UINavigationController(rootViewController: landingViewController)
