082624961b7eca7013f14581621c74a24b5bd79d
Julian Hulme <julian@arcadesoftware.io>
2016-05-18 11:40:44 +0200

    created the login screen (via add file etc)

---
+++ Onboarding/screens/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Onboarding
+//
+//  Created by Julian Hulme on 2016/05/18.
+//  Copyright © 2016 Julian Hulme. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
